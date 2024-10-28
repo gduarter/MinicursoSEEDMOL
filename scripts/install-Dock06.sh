2dbcb551632de2065e91b42668a71b4763db65b7
NewtonSheep <170121186@aluno.unb.br>
2024-10-28 13:24:53 -0300

    Update install-Dock06.sh
    Polishing

--- scripts/install-Dock06.sh
+++ scripts/install-Dock06.sh
@@ -2,8 +2,8 @@ sudo apt update
 sudo apt install -y gcc g++ gfortran
 
 cd ~/local
-tar -xvf dock06.tar.gz
-rm -rf doc06.tar.gz
+tar -xvf dock06.tar
+rm -rf doc06.tar
 cd ~/local/dock06/install
 ./configure gnu
 make install dock
